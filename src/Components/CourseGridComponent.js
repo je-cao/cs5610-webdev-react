5e3d32a32b2d21dffbf115510cf5570b58c2502e
zhixua <cao.zhix@northeastern.edu>
2020-10-13 14:56:25 -0700

    A4

--- src/Components/CourseGridComponent.js
+++ src/Components/CourseGridComponent.js
@@ -5,96 +5,74 @@ import {Container, Card, Button } from 'react-bootstrap';
 
 const CourseGridComponent = () => {
     return (
-        <div>
-            <div className="card-deck">
-                <h1>
-                    Course Manager <input placeholder="New Course Title" /> <button className="btn-dark"> + </button>
-                </h1>
+        <div className="container">
+            <h1>
+                Course Manager <input placeholder="New Course Title" /> <button className="btn-dark"> + </button>
+            </h1>
+            <table className="table">
+                <thead>
+                <tr>
+                    <th>Recent Document</th>
+                    <th>Owned by me</th>
+                </tr>
+                </thead>
+            </table>
 
-                <table className="table">
-                    <thead>
-                    <tr className="wbdv-button">
-                        <th>Recent Document</th>
-                        <th>Owned by me</th>
-                    </tr>
-                    </thead>
-                </table>
-
-                <Card style={{ width: '18rem' }} >
-                    <Card.Img variant="top d-flex" src="https://picsum.photos/200/300"/>
-                    <Card.Body>
-                        <Card.Title>Web Development</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                        <Button variant="primary">Edit</Button>
-                    </Card.Body>
-                </Card >
-
-                <Card style={{ width: '18rem' }} >
-                    <Card.Img variant="top d-flex" src="https://picsum.photos/200/300"/>
-                    <Card.Body>
-                        <Card.Title>Database Management</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                        <Button variant="primary">Edit</Button>
-                    </Card.Body>
-                </Card >
-
-                <Card style={{ width: '18rem' }} >
-                    <Card.Img variant="top d-flex" src="https://picsum.photos/200/300"/>
-                    <Card.Body>
-                        <Card.Title>Data Structure</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                        <Button variant="primary">Edit</Button>
-                    </Card.Body>
-                </Card >
-
-            </div>
-
-            <div className="card-deck">
-                <Card style={{ width: '18rem' }} >
-                    <Card.Img variant="top d-flex" src="https://picsum.photos/200/300"/>
-                    <Card.Body>
-                        <Card.Title>Web Development</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                        <Button variant="primary">Edit</Button>
-                    </Card.Body>
-                </Card >
-
-                <Card style={{ width: '18rem' }} >
-                    <Card.Img variant="top d-flex" src="https://picsum.photos/200/300"/>
-                    <Card.Body>
-                        <Card.Title>Database Management</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                        <Button variant="primary">Edit</Button>
-                    </Card.Body>
-                </Card >
-
-                <Card style={{ width: '18rem' }} >
-                    <Card.Img variant="top d-flex" src="https://picsum.photos/200/300"/>
-                    <Card.Body>
-                        <Card.Title>Data Structure</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                        <Button variant="primary">Edit</Button>
-                    </Card.Body>
-                </Card >
-
+            <div className="row">
+                <div className="col-xs-1 col-sm-2 col-md-3 col-lg-4 col-xl-6 ">
+                    <div className="card">
+                        <Card.Body>
+                            <Card.Img variant="top d-flex" src="https://picsum.photos/200/300"/>
+                            <Card.Title>Web Development</Card.Title>
+                            <Button variant="primary">Edit</Button>
+                        </Card.Body>
+                    </div>
+                </div>
+                <div className="cal-12 col-xs-1 col-sm-2 col-md-3 col-lg-4 col-xl-6 ">
+                    <div className="card">
+                        <Card.Body>
+                            <Card.Img variant="top d-flex" src="https://picsum.photos/200/300"/>
+                            <Card.Title>Web Development</Card.Title>
+                            <Button variant="primary">Edit</Button>
+                        </Card.Body>
+                    </div>
+                </div>
+                <div className="cal-12 col-xs-1 col-sm-2 col-md-3 col-lg-4 col-xl-6 ">
+                    <div className="card">
+                        <Card.Body>
+                            <Card.Img variant="top d-flex" src="https://picsum.photos/200/300"/>
+                            <Card.Title>Web Development</Card.Title>
+                            <Button variant="primary">Edit</Button>
+                        </Card.Body>
+                    </div>
+                </div>
+                <div className="cal-12 col-xs-1 col-sm-2 col-md-3 col-lg-4 col-xl-6 ">
+                    <div className="card">
+                        <Card.Body>
+                            <Card.Img variant="top d-flex" src="https://picsum.photos/200/300"/>
+                            <Card.Title>Web Development</Card.Title>
+                            <Button variant="primary">Edit</Button>
+                        </Card.Body>
+                    </div>
+                </div>
+                <div className="cal-12 col-xs-1 col-sm-2 col-md-3 col-lg-4 col-xl-6 ">
+                    <div className="card">
+                        <Card.Body>
+                            <Card.Img variant="top d-flex" src="https://picsum.photos/200/300"/>
+                            <Card.Title>Web Development</Card.Title>
+                            <Button variant="primary">Edit</Button>
+                        </Card.Body>
+                    </div>
+                </div>
+                <div className="cal-12 col-xs-1 col-sm-2 col-md-3 col-lg-4 col-xl-6 ">
+                    <div className="card">
+                        <Card.Body>
+                            <Card.Img variant="top d-flex" src="https://picsum.photos/200/300"/>
+                            <Card.Title>Web Development</Card.Title>
+                            <Button variant="primary">Edit</Button>
+                        </Card.Body>
+                    </div>
+                </div>
             </div>
         </div>
 
